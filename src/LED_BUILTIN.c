45abac035095f7214088d6641335e1065cb2c636
Dominic Martin <m.dominic@pmx-group.com>
2026-04-14 14:48:45 +0200

    added 2 LED modes

--- src/LED_BUILTIN.c
+++ src/LED_BUILTIN.c
@@ -1,5 +1,4 @@
 #define F_CPU 16000000UL //Definiert die Taktfrequenz für die Zeitrechnung (delay_ms)
-
 #include <avr/io.h> //internes Headerfile, stellt Symbolnamen wie PORTB, PIN etc. bereit
 #include <util/delay.h> //internes Headerfile, stell avr-gcc (Compiler später bereit) --> richtige Mikrocontroller Programmierung
 #include <stdint.h> //HeaderDatei für uint usw. 
@@ -8,12 +7,39 @@
 #define LEDON (PORTB |= (1<<PB5))
 #define LEDOFF (PORTB &= ~(1 <<PB5))
 
+
+typedef enum {
+    MODE1 = 0,
+    MODE2 = 1
+} led_mode_t;
+
+
+
+void led_modes (led_mode_t MODUS){
+	switch(MODUS)
+	{
+		case MODE1:
+			LEDON; _delay_ms(1100);
+			LEDOFF; _delay_ms(1100);
+			break;
+
+		case MODE2:
+			for (uint8_t i=0; i<3; i++) {
+					LEDON;  _delay_ms(100);
+					LEDOFF; _delay_ms(100);
+			}
+			_delay_ms(1000);
+			break;
+	}
+}
+
+
+
 int main(void)
 {
 	DDRB |= (1 << DDB5); //Das hier definiert Pin13 als Ausgabe
-
 	while(1){
-		LEDON; _delay_ms(800);
-		LEDOFF; _delay_ms(800); 	
+		led_modes(MODE2);
 	}
+	
 }
