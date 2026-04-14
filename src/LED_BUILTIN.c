182705b0e2ee110be6a94e4009afcd225851d9af
Dominic Martin <m.dominic@pmx-group.com>
2026-04-14 14:09:25 +0200

    still fixin stash/unstash

--- src/LED_BUILTIN.c
+++ src/LED_BUILTIN.c
@@ -1,7 +1,9 @@
+#define F_CPU 16000000UL //Definiert die Taktfrequenz für die Zeitrechnung (delay_ms)
+
 #include <avr/io.h> //internes Headerfile, stellt Symbolnamen wie PORTB, PIN etc. bereit
 #include <util/delay.h> //internes Headerfile, stell avr-gcc (Compiler später bereit) --> richtige Mikrocontroller Programmierung
 #include <stdint.h> //HeaderDatei für uint usw. 
-#define F_CPU 16000000UL //Definiert die Taktfrequenz für die Zeitrechnung (delay_ms)
+
 
 #define LEDON (PORTB |= (1<<PB5))
 #define LEDOFF (PORTB &= ~(1 <<PB5))
@@ -10,6 +12,8 @@ int main(void)
 {
 	DDRB |= (1 << DDB5); //Das hier definiert Pin13 als Ausgabe
 
-	LEDON; _delay_ms(800);
-	LEDOFF; _delay_ms(800); 
+	while(1){
+		LEDON; _delay_ms(800);
+		LEDOFF; _delay_ms(800); 	
+	}
 }
